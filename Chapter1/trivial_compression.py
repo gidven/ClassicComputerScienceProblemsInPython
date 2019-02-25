4006507d8aae5f1b9898dfcc179410db39522230
Gid <giddevanderven@gmail.com>
2019-02-25 20:34:14 +0100

    Update trivial_compression.py
    flake8 proofing

--- Chapter1/trivial_compression.py
+++ Chapter1/trivial_compression.py
@@ -36,7 +36,8 @@ class CompressedGene:
 
     def decompress(self) -> str:
         gene: str = ""
-        for i in range(0, self.bit_string.bit_length() - 1, 2):  # - 1 to exclude sentinel
+        # -1 to compensate for sentinel
+        for i in range(0, self.bit_string.bit_length() - 1, 2):
             bits: int = self.bit_string >> i & 0b11  # get just 2 relevant bits
             if bits == 0b00:  # A
                 gene += "A"
@@ -56,9 +57,10 @@ class CompressedGene:
 
 if __name__ == "__main__":
     from sys import getsizeof
-    original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATATAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATA" * 100
+    original: str = "TAGGGATTAACCGTTATATATATATAGCCATGGATCGATTATCTCCAAATT" * 200
     print("original is {} bytes".format(getsizeof(original)))
     compressed: CompressedGene = CompressedGene(original)  # compress
     print("compressed is {} bytes".format(getsizeof(compressed.bit_string)))
     print(compressed)  # decompress
-    print("original and decompressed are the same: {}".format(original == compressed.decompress()))
+    print("original and decompressed are the same: {}"
+          .format(original == compressed.decompress()))
